eaae18db1c3b308b5546a71d601c47bea67d327a
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-07-26 10:02:33 -0600

    finished ring buffer - tests pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,11 +5,17 @@ class RingBuffer:
         self.storage = [None]*capacity
 
     def append(self, item):
+        # Starts with the next oldest space, evaluates, then advances pointer to next space.
+        # Pointer resets to 0 when pointer is greater than capacity.
         self.storage.pop(self.current)
         self.storage.insert(self.current, item)
         self.current += 1
-        if self.current > self.capacity:
+        if self.current > self.capacity - 1:
             self.current = 0
 
     def get(self):
-        pass
+        no_nones = []
+        for item in self.storage:
+            if item:
+                no_nones.append(item)
+        return no_nones
